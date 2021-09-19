003d64e6e83d59ad49bc9af8ad78686e10f7fa36
Renan-A-Lira <renanalmeida2019.ra@gmail.com>
2021-09-19 17:37:51 -0300

    like add

--- blog/static/js/data.js
+++ blog/static/js/data.js
@@ -1,3 +1,33 @@
+var doubletapDeltaTime_ = 700;
+var doubletap1Function_ = null;
+var doubletap2Function_ = null;
+var doubletapTimer = null;
+
+function tap(singleTapFunc, doubleTapFunc, id) {
+    if (doubletapTimer==null) {
+    // First tap, we wait X ms to the second tap
+        doubletapTimer = setTimeout(() =>{
+            doubletapTimeout()
+        }, doubletapDeltaTime_);
+        doubletap1Function_ = singleTapFunc;
+        doubletap2Function_ = doubleTapFunc;
+    } else {
+    // Second tap
+        clearTimeout(doubletapTimer);
+        doubletapTimer = null;
+        doubletap2Function_(id);
+    }
+}
+
+function doubletapTimeout() {
+// Wait for second tap timeout
+    doubletap1Function_;
+    doubleTapTimer = null;
+}
+
+
+
+
 function setlike(id){
 
     let data = {
@@ -26,15 +56,20 @@ function setLikesCount(count_like, action, image_id){
     likes.innerHTML = `${count_like} curtidas`
     let icon_like = document.getElementById(`like-image-${image_id}`)
     let icon_deslike = document.getElementById(`notlike-image-${image_id}`)
+    let icon_heart_like_img = document.getElementById(`heart-like-img-${image_id}`)
     if (action){
         icon_like.style.display='flex'
         icon_deslike.style.display='none'
+        icon_heart_like_img.style.fontSize = '8rem'
+        setTimeout(()=>{
+            icon_heart_like_img.style.fontSize='0'
+        }, 1000)
     } else {
         icon_like.style.display='none'
         icon_deslike.style.display='flex'
+        icon_heart_like_img.style.fontSize='0'
     }
 }
-
 
 
 function getCookie(name) {
